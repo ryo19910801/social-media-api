99476ae0ac1ce64daa19b1da40a809be33b7ecc0
ryo.naruse <naruse19910801@gmail.com>
2020-06-14 13:42:14 +0900

    :sparkles: Add trait to user spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,11 @@ FactoryBot.define do
     name { "hoge" }
     self_introduction { "よろしく" }
     website_url { "https://google.com" }
+
+    trait :with_avatar do
+      after(:build) do |user|
+        user.avatar = fixture_file_upload(Rails.root.join('spec', 'files', 'avatar.png'), 'image/png')
+      end
+    end
   end
 end
