b69356cd30dd01163e1112a0fd05c95e8715e5f2
ryo.naruse <naruse19910801@gmail.com>
2020-06-15 09:06:44 +0900

    :sparkles: Add authenticate

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,11 +1,27 @@
 module Api
   module V1
     class ApiController < ApplicationController
+      include ActionController::HttpAuthentication::Token::ControllerMethods
       protect_from_forgery
 
-      # 400 Bad Request
+      before_action :authenticate!
+
+      helper_method :current_user
+
       def response_bad_request
         render status: 400, json: { status: 400, message: 'Bad Request' }
+      end
+
+      private
+      def authenticate!
+        # Authorization header Token hoge
+        authenticate_or_request_with_http_token do |token, options|
+          User.find_by(auth_token: token).present?
+        end
+      end
+
+      def current_user
+        @current_user ||= User.find_by(auth_token: request.headers['Authorization'].split[1])
       end
     end
   end
