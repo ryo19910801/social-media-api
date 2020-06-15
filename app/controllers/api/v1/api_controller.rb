1349bd89deea7a4fe156e5ee4d7f9c477f75e296
ryo.naruse <naruse19910801@gmail.com>
2020-06-15 17:38:13 +0900

    Add request user spec

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -3,20 +3,25 @@ module Api
     class ApiController < ApplicationController
       include ActionController::HttpAuthentication::Token::ControllerMethods
       protect_from_forgery
-
       before_action :authenticate!
 
       helper_method :current_user
 
+      rescue_from Exception, with: :response_not_found
+
       def response_bad_request
         render status: 400, json: { status: 400, message: 'Bad Request' }
+      end
+
+      def response_not_found
+        render status: 404, json: { status: 404, message: "Not Found" }
       end
 
       private
       def authenticate!
         # Authorization header Token hoge
         authenticate_or_request_with_http_token do |token, options|
-          User.find_by(auth_token: token).present?
+          @current_user = User.find_by(auth_token: token)
         end
       end
 
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -32,4 +32,33 @@ RSpec.describe "Users", type: :request do
       end
     end
   end
+
+  describe "PATCH /users" do
+    let!(:user) { create(:user) }
+    let(:user_params) { attributes_for(:user) }
+    context "is valid parameters" do
+      it "return status 200" do
+        user_params[:id] = user.id
+        # TODO shared_context Authorization
+        patch '/api/v1/users/', params: { user: user_params }, headers: { "Authorization" => "Token token=#{user.auth_token}" }
+        expect(response).to have_http_status(200)
+      end
+      it "is changed column" do
+        user_params[:id] = user.id
+        user_params[:name] = "changed_name"
+        patch '/api/v1/users/', params: { user: user_params }, headers: { "Authorization" => "Token token=#{user.auth_token}" }
+        json = JSON.parse(response.body)
+        user.reload
+        expect(json["user"]["name"]).to eq user.name
+      end
+    end
+    context "is invalid parameters" do
+      it "return status 404" do
+        user_params[:id] = user.id
+        user_params[:name] = nil
+        patch '/api/v1/users/', params: { user: user_params }, headers: { "Authorization" => "Token token=#{user.auth_token}" }
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
 end
