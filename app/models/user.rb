ae4820df6e21db094816e19ddd3329865f59d872
ryo.naruse <naruse19910801@gmail.com>
2020-06-15 15:53:28 +0900

    Add rspec user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   has_secure_token :auth_token
   has_one_attached :avatar
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :self_introduction, length: { maximum: 255 }
+  validates :website_url, length: { maximum: 255 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,54 @@ RSpec.describe User, type: :model do
     expect(invalid_user.errors[:name]).to include("can't be blank")
   end
 
+  it "is valid if name characters less than 255" do
+    user = build(:user, name: "a" * 200)
+    expect(user).to be_valid
+  end
+
+  it "is valid if name characters equal 255" do
+    user = build(:user, name: "a" * 255)
+    expect(user).to be_valid
+  end
+
+  it "is invalid if name characters greater than 256" do
+    user = build(:user, name: "a" * 256)
+    user.valid?
+    expect(user.errors[:name]).to include("is too long (maximum is 255 characters)")
+  end
+
+  it "is valid if self_introduction characters less than 255" do
+    user = build(:user, self_introduction: "a" * 200)
+    expect(user).to be_valid
+  end
+
+  it "is valid if self_introduction characters equal 255" do
+    user = build(:user, self_introduction: "a" * 255)
+    expect(user).to be_valid
+  end
+
+  it "is invalid if self_introduction characters greater than 256" do
+    user = build(:user, self_introduction: "a" * 256)
+    user.valid?
+    expect(user.errors[:self_introduction]).to include("is too long (maximum is 255 characters)")
+  end
+
+  it "is valid if website_url characters less than 255" do
+    user = build(:user, website_url: "a" * 200)
+    expect(user).to be_valid
+  end
+
+  it "is valid if website_url characters equal 255" do
+    user = build(:user, website_url: "a" * 255)
+    expect(user).to be_valid
+  end
+
+  it "is invalid if website_url characters greater than 256" do
+    user = build(:user, website_url: "a" * 256)
+    user.valid?
+    expect(user.errors[:website_url]).to include("is too long (maximum is 255 characters)")
+  end
+
   it "is attach avatar file" do
     file = fixture_file_upload(Rails.root.join('spec', 'files', 'avatar.png'), 'image/png')
     valid_user.avatar = file
